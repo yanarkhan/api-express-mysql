a8ab501e32269f0d71f4224fa5354ab4c1181a68
yanarkhan <rianarkhan@gmail.com>
2024-02-12 03:10:20 +0700

    setup environment variable di express

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "lat_api",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = dbPool.promise();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 // Import Outfile
 const usersRoutes = require("./routes/users");
